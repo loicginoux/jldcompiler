255a15cfd32d504a9923c726ccb5353e7c59ad2d
bdav24 <bdav24@0f5709b6-2b4d-0410-b15a-05248fe13f3c>
2008-05-22 12:53:48 +0000

    TDT

--- mjc/tdt/TDT.java
+++ mjc/tdt/TDT.java
@@ -6,16 +6,10 @@ public class TDT extends HashMap<String, INFO> {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private static HashMap<String, INFO> tdt;
-	
 	public TDT() {
-		if(true) { //le fichier avec les TDT existantes n'existe pas
-			tdt = new HashMap<String, INFO>();
-			tdt.put("int", new INFO(1,"NULL"));
-			tdt.put("boolean", new INFO(1,"NULL"));
-		} else {
-			//ouvrir le fichier et charger la table...
-		}
+		super();
+		put("int", new INFO(1,"NULL"));
+		put("boolean", new INFO(1,"NULL"));
 	}
 	
 	public INFO chercher(String n) {
@@ -26,18 +20,14 @@ public class TDT extends HashMap<String, INFO> {
 		put(n, i);
 	}
 
-	public static HashMap<String, INFO> getTdt() {
-		return tdt;
-	}
-
-	public static void setTdt(HashMap<String, INFO> tdt) {
-		TDT.tdt = tdt;
+	public INFO getInfo(String nom) {
+		return get(nom);
 	}
 	
 	public String toString() {
 		String res = "";
-		for(String nomClasse : tdt.keySet()) {
-			res += "Class : " + nomClasse + tdt.get(nomClasse).toString();
+		for(String nomClasse : this.keySet()) {
+			res += "Class : " + nomClasse + this.get(nomClasse).toString();
 		}
 		return res;
 	}
