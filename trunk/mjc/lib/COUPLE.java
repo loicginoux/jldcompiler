7298445db554631eca49526e4c539b5cb7a03d40
jnoleau <jnoleau@0f5709b6-2b4d-0410-b15a-05248fe13f3c>
2008-05-22 09:33:10 +0000

    lala

---
+++ trunk/mjc/lib/COUPLE.java
@@ -0,0 +1,36 @@
+package mjc.lib;
+
+public class COUPLE {
+	private int gauche;
+	private boolean droite;
+
+	
+
+	public int getGauche() {
+		return gauche;
+	}
+
+	public void setGauche(int gauche) {
+		this.gauche = gauche;
+	}
+
+	public boolean isDroite() {
+		return droite;
+	}
+
+	public void setDroite(boolean droite) {
+		this.droite = droite;
+	}
+
+
+
+	public COUPLE(int gauche, boolean droite) {
+		super();
+		this.gauche = gauche;
+		this.droite = droite;
+	}
+
+	public String toString() {
+		return  "<" + gauche + ", " + droite + ">";
+	}
+}
