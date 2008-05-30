9d6b7ca6d61d08dbdf4cf9e48e0a380b6c2d3201
bdav24 <bdav24@0f5709b6-2b4d-0410-b15a-05248fe13f3c>
2008-05-30 10:35:25 +0000

    gestion surcharge

--- mjc/tdm/Signature.java
+++ mjc/tdm/Signature.java
@@ -64,7 +64,55 @@ public class Signature implements Serializable {
 			resParam+= p+ ",";
 		}
 		
-		return "INFO : " + visi + " " + stat + " " + retour +"\n parametre:" + resParam.substring(0, resParam.length()-1); 
+		return "INFO : " + visi + " " + stat + " " + retour +"\n parametre:" + resParam.substring(0, resParam.length()-1);
+	}
+	
+	public boolean equalsBis(Object o){
+
+		if (o instanceof Signature) {
+			Signature s = (Signature) o;
+			
+			
+			
+			if(s.params.size() != this.params.size()) return false;
+			else{
+				
+				for(int i = 0; i < params.size(); i++){
+					String ss = params.get(i);
+					String ss2 = s.getParams().get(i);
+					
+					System.out.println("ss:"+ss+ "\n");
+					System.out.println("ss2:"+ss2+ "\n");
+					System.out.println("\n"+ss.equals(ss2));
+
+					if(!ss.equals(ss2)) return false;
+				}
+				
+				return true;
+				
+			}
+			
+		}else
+			return false;
+		
+	}
+
+	public List<String> getParams() {
+		return params;
+	}
+
+	public void setParams(List<String> params) {
+		this.params = params;
+	}
+	
+	public String getStringParams() {
+		List<String> list=this.params;
+		
+		String res = "";
+		for(int i=0; i<params.size(); i++){
+		res +=  list.get(i);
+		}
+		return res;
 	}
 
 }
