ac2d2857e5d87324eec3ef1fee8fae43d9d1a0c3
bdav24 <bdav24@0f5709b6-2b4d-0410-b15a-05248fe13f3c>
2008-05-31 13:43:06 +0000

    debut de generation de code

--- mjc/gc/TAM.java
+++ mjc/gc/TAM.java
@@ -1,8 +1,124 @@
 package mjc.gc;
 
-public class TAM extends AbstractMachine {
+import mjc.tdm.TDM;
+import mjc.tds.INFO;
+import mjc.tdt.TDT;
 
-	public String getSuffixe() {
+
+public class TAM extends AbstractMachine {
+	
+	private static int n = 0;
+
+	/*
+	 * Pour avoir un truc un peu plus parlant qu'une etiquette
+	 */
+	public String genLabel(String label) {
+		return label + "_" + n++;
+	}
+	
+	/*
+	 * TODO : reflechir ^^
+	 */
+//	public static String genDecl(String n, INFO i, int taille) {
+//        return "\tPUSH " + taille + "   ; decl de " + n + " en " + i.getDep()
+//          + "/SB" + " taille = " + taille + "\n";
+//    }
+//    
+//    public static String genStore(String n, INFO i, int taille) {
+//    	return "\tSTORE(" + taille + ") " + i.getDep() + "[SB]; sauvegarde\n";
+//    }
+//    
+//    public static String genLoadIdent(String var, INFO i, int taille) {
+//    	return "\tLOAD(" + taille + ") " + i.getDep() + "[SB] ; " + var + "\n";
+//    }
+	
+    public static String genPop(int taille1, int taille2){
+    	return "\tPOP(" + taille1 + ") " + taille2 + "\n";
+    }
+    
+    public String genCond(String codeBloc1, String codeBloc2){
+    	String et1 = genEtiq();
+    	String res;
+    	if (codeBloc2.equals("")) {
+    		res = "\tJUMPIF(0) " + et1 + ";si \n" + 
+    				codeBloc1 +
+    				et1 + "\t;fin si\n";
+    	} else {
+    		String et2 = genEtiq();
+    		res = "\tJUMPIF(0) " + et1 + ";si \n" + 
+		        	codeBloc1 + 
+		        	"\tJUMP " + et2 + "\n" + 
+		        	et1 + "\t;sinon\n" + 
+		        	codeBloc2 +
+		        	et2 + "\t;fin si\n";
+    	}
+    	return res;
+    }
+    
+    public String genWhile(String condition, String bloc) {
+		String startWhile = genEtiq();
+		String endWhile = genEtiq();
+
+		return startWhile + "\n" + condition + "\t" + "JUMPIF (0) "
+				+ endWhile + "\n" + bloc + "\t" + "JUMP " + startWhile + "\n"
+				+ endWhile + "\n";
+	}
+    
+    public String genReturn(String nomMethode, TDM tdm) {
+    	System.out.println(nomMethode);
+    	int size = tdm.chercherGlobalement(nomMethode).getParams().size();
+    	String res = "\t" + "RETURN (1) " + (size + 1) +
+    					"\t;on quitte " + nomMethode + "\n";
+		return res;
+	}
+    
+    public static String genLoadL(String value){
+    	return "\tLOADL " + value + "\n";
+    }
+    
+    
+    public String genTrue() {
+		return "\tLOADL 1\n";
+	}
+    
+    public String genFalse() {
+		return "\tLOADL 0\n";
+	}
+    
+    public String genNull() {
+		return "\tLOADL 0\n";
+	}
+    
+    public String genInt(String i) {
+		return "\tLOADL " + i + "\n";
+	}
+    
+    public String genPlusInt() {
+		return "";
+	}
+    
+    public String genMoinsInt() {
+		return "\tSUBR INeg \n";
+	}
+    
+    public String genNegation() {
+		return "\tSUBR BNeg\n";
+	}
+	
+	// compteur pour le generateur d'etiquettes
+    private static int cpt = 0;
+
+    // genere une etiquette differente des autres
+    public static String genEtiq() {
+        return "X" + cpt++;
+    }
+
+ // genere le code pour l'arret de la machine
+    public static String genFin() {
+        return "\tHALT\n";
+    }
+    
+    public String getSuffixe() {
 		return "tam";
 	}
 
--- mjc/tdm/Signature.java
+++ mjc/tdm/Signature.java
@@ -7,6 +7,7 @@ import java.util.Vector;
 
 public class Signature implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	public final static int PUBLIC = 0;
 	public final static int PRIVATE = 1;
 	public final static int PROTECTED = 2;
