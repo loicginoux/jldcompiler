f1c930f0f2ca9864f743165c907670fc19b90ba3
bdav24 <bdav24@0f5709b6-2b4d-0410-b15a-05248fe13f3c>
2008-06-04 14:45:47 +0000

    ajout de affectable

---
+++ mjc/lib/Affectable.java
@@ -0,0 +1,21 @@
+package mjc.lib;
+
+public class Affectable {
+
+	private boolean isAffectable;
+	
+	public Affectable(boolean b){
+		isAffectable = b;
+	}
+
+	public boolean isAffectable() {
+		return isAffectable;
+	}
+
+	public void setAffectable(boolean isAffectable) {
+		this.isAffectable = isAffectable;
+	}
+	
+	
+	
+}
