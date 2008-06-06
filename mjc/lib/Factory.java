569d552ac93a33c7b3c1ae0194269082da35287f
bdav24 <bdav24@0f5709b6-2b4d-0410-b15a-05248fe13f3c>
2008-06-06 14:09:25 +0000

    imports fixed

--- mjc/lib/Factory.java
+++ mjc/lib/Factory.java
@@ -7,7 +7,9 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Vector;
 
+import mjc.gc.AbstractMachine;
 import mjc.tdm.TDM;
 import mjc.tds.TDS;
 import mjc.tdt.TDT;
@@ -15,6 +17,7 @@ import mjc.tdt.TDT;
 public class Factory {
 	
 	private static String langage;
+	private static Vector<String> imports;
 
 	public static void persister(Serializable t,String filename){
 		try{
@@ -96,8 +99,25 @@ public class Factory {
 		}
 	}
 	
-	public static String genLinkageImports(TDT tdt){
-		return "\t\t; NOT IMPLANTED link imports\n";
+	public static String genLinkageImports(TDT tdt, AbstractMachine machine) {
+		imports = new Vector<String>();
+		getClassImported(tdt);
+		imports.remove(tdt.getClassname());
+		String res = "";
+		for (String classToImport : imports) {
+			res += chargerCode(classToImport + "_code");
+		}
+		return "\t\t; imports\n" + res + "\t\t; fin imports\n\n";
+	}
+	
+	private static void getClassImported(TDT tdt) {
+		if (!imports.contains(tdt.getClassname()))
+			imports.add(tdt.getClassname());
+		if (tdt.getParentes() != null) {
+			for (TDT t : tdt.getParentes()) {
+				getClassImported(t);
+			}
+		}
 	}
 	
 	public static boolean isNull(Object o){
